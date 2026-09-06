3b6f3ec378722de951053fe50f664a7a1a09d6f4
Sakshi Agrawal <sak07.agg@gmail.com>
2026-09-06 14:09:33 +0530

    Sakshi added Week 1 assignment problems

---
+++ src/main/java/string/assignment_problems/MovieReviewWordProfiler.java
@@ -0,0 +1,37 @@
+package string.assignment_problems;
+import java.util.Scanner;
+public class MovieReviewWordProfiler {
+    static void classifyWordLengths(String review) {
+
+        String[] words = review.split(" ");
+
+        int shortWords = 0;
+        int mediumWords = 0;
+        int longWords = 0;
+
+        for (String word : words) {
+
+            int length = word.length();
+
+            if (length <= 4)
+                shortWords++;
+            else if (length <= 8)
+                mediumWords++;
+            else
+                longWords++;
+        }
+
+        System.out.println("Short: " + shortWords);
+        System.out.println("Medium: " + mediumWords);
+        System.out.println("Long: " + longWords);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter movie review: ");
+        String review = sc.nextLine();
+
+        classifyWordLengths(review);
+    }}
